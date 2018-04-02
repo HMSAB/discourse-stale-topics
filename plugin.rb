a59b23803f245b784bc93e360f5d462ef59b20ad
jose-hms <jose@hms.se>
2018-04-02 14:41:35 -0400

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: stale-topics
+# about: Ensure stale topics are followed-up on
+# version: 0.1
+# authors: Jordan Seanor
+# url: https://github.com/HMSAB/discourse-private-topic.git
+
+enabled_site_setting :stale_topics_enabled
